8e1fef2b1659af649d15f5023bc7db8c5ff70d13
Jorge Vergara <jorge@jorgevergara.co>
2024-10-13 17:40:10 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'party-planner',
+  webDir: 'www'
+};
+
+export default config;
